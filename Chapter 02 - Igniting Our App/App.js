5b50164f32216bbb60a4e8d33666a5490f284c16
ayush14019 <rajayush224@gmail.com>
2025-08-08 15:12:58 +0530

    Chapter 2 completed

---
+++ Chapter 02 - Igniting Our App/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+/*
+Manipulate the HTML DOM using Javscript
+const heading = document.createElement("h1");
+heading.innerHTML = "Namaste Everyone";
+const root = document.getElementById("root");
+root.appendChild(heading);
+*/
+
+// Manipulate the HTML DOM using React
+
+// creating Nested React Elements Object
+/*
+ * <div id="parent">
+ *   <div id="child">
+ *     <h1>I am an h1 tag</h1>
+ *   </div>
+ * </div>
+ *
+ * ReactElement(Object) => HTML(Browser Understands)
+ */
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm the h1 tag"),
+    React.createElement("h2", {}, "I'm the h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm the h1 tag"),
+    React.createElement("h2", {}, "I'm the h2 tag"),
+  ]),
+);
+
+console.log(heading);
+// create root using createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside root
+root.render(heading);
+
+// const heading = React.createElement("h1", {}, "Hello World!");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
---
+++ Chapter 02 - Igniting Our App/README.md
@@ -0,0 +1,20 @@
+# Namaste-react
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR =Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Build
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistant Hashing
+- Code Splitting
+- Differntial Bundling - support older broswer
+- Diagnotics
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- different dev and prod bundles
